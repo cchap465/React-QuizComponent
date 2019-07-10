28138d3690c390105e2ff880002bdbc57862667d
Crissy Miller <cmiller2@paylocity.com>
2019-07-10 11:30:26 -0500

    Completed the module

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -5,13 +5,13 @@ class QuizEnd extends Component {
         return (
             <div>
                 <p>Thanks for playing!</p>
-                <a onClick={this.handlerResetClick.bind(this)} href=''>Reset Quiz</a>
+                <a onClick={this.handleResetClick.bind(this)} href=''>Reset Quiz</a>
             </div>
         )
     }
 
-    handlerResetClick() {
-
+    handleResetClick() {
+        this.props.resetClickHandler()
     }
 }
 
